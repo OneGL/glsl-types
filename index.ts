7f3b919339cacd3b01af917444719df36be0055a
Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>
2024-05-30 06:00:41 -0400

    Adds commander and watch mode

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 import path from "path";
 import chalk from "chalk";
 import glsl_types from "./pkg/glsl_types.js";
+import { program } from "commander";
 
 global.console_log = (message) => console.log(message);
 global.log_with_color = (message, color) => {
@@ -13,8 +14,34 @@ global.file_exists = (file) => fs.existsSync(file);
 global.create_dir_all = (dir) => fs.mkdirSync(dir, { recursive: true });
 global.write_file = (file, content) => fs.writeFileSync(file, content);
 
-glsl_types.start_cli(
-  "/home/luis/github/onegl/glsl-types/shaders/program1.vert",
-  "./shaders",
-  "./output2"
-);
+program
+  .option("-i, --input <input>", "Input directory", "./shaders")
+  .option("-o, --output <output>", "Output directory", "./output")
+  .option("-w, --watch", "Watch for changes", false);
+
+program.parse();
+
+const SHADER_EXTENSIONS = [".vert", ".frag", ".vs", ".fs", ".glsl"];
+
+const options = program.opts();
+
+if (options.watch) {
+  process.stdout.write(chalk["green"]("Watching for changes\n"));
+  fs.watch(options.input, { recursive: true }, (eventType, filename) => {
+    if (!filename) return;
+
+    if (SHADER_EXTENSIONS.includes(path.extname(filename))) {
+      glsl_types.start_cli(
+        "/home/luis/github/onegl/glsl-types/shaders/program1.vert",
+        options.input,
+        options.output
+      );
+    }
+  });
+} else {
+  glsl_types.start_cli(
+    "/home/luis/github/onegl/glsl-types/shaders/program1.vert",
+    options.input,
+    options.output
+  );
+}
